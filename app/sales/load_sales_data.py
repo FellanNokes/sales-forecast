a676bb24b9db640da0b44257ec325c7e660f93b3
Miranda Lyng <lyngmiranda@gmail.com>
2026-03-11 10:36:33 +0100

    Refactor: Constructed the load-data file to a function and added parameters for the csv-file-name and table-name

--- app/sales/load_sales_data.py
+++ app/sales/load_sales_data.py
@@ -4,21 +4,25 @@ from dotenv import load_dotenv
 import os
 
 
-# Read .env
-load_dotenv()
+def load_sales_data(csv_path: str, table_name: str) -> list[dict]:
+    # Read .env
+    load_dotenv()
+
+    # url
+    url = os.getenv("SUPABASE_URL")
+    # key
+    key = os.getenv("SUPABASE_KEY")
+
+    # connect to superbase project with Client
+    supabase: Client = create_client(url, key)
+
+    # read the cleaned sales data
+    df = pd.read_csv(csv_path, sep="|")
+
+    records = df.to_dict(orient="records")
+    response = supabase.table(table_name).insert(records).execute()
+
+    return response.data
 
 
-# url
-url = os.getenv("SUPABASE_URL")
-# key
-key = os.getenv("SUPABASE_KEY")
-
-# connect to superbase project with Client
-supabase: Client = create_client(url, key)
-
-# read the cleaned sales data
-df = pd.read_csv("csv_cleaned_file", sep="|")
-
-
-records = df.to_dict(orient="records")
-supabase.table("taable_name").insert(records).execute()
+result = load_sales_data(csv_path, table_name)
